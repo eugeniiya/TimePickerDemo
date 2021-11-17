23414b22e69e301d57bfb1e8d468145bd27ab4c1
eugeniiya <evgenia.fonina.2013@yandex.ru>
2021-11-17 19:32:47 +0300

    Добавьте файлы проекта.

---
+++ TimePickerDemo/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TimePickerDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        DateTime TT;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Device.StartTimer(TimeSpan.FromSeconds(1), OnTimerTick);
+        }
+        bool OnTimerTick()
+        {
+            if (Sw.IsToggled && DateTime.Now >= TT)
+            {
+                Sw.IsToggled = false;
+                DisplayAlert("Timer Alert",
+                "The '" + Ent.Text + "' timer has elapsed", "OK");
+            }
+            return true;
+        }
+        void OnTimePickerChanged(object sender,
+        PropertyChangedEventArgs args)
+        {
+            if (args.PropertyName == "Time")
+            {
+                SetTriggerTime();
+            }
+        }
+        void OnSwitchToggled(object sender, ToggledEventArgs args)
+        {
+            SetTriggerTime();
+        }
+        void SetTriggerTime()
+        {
+            if (Sw.IsToggled)
+            {
+                TT = DateTime.Today + TmPicker.Time;
+                if (TT < DateTime.Now)
+                {
+                    TT += TimeSpan.FromDays(1);
+                }
+            }
+        }
+    }
+}
